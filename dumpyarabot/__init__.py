120b6ce0477f36ed3c59d7890929aa04bb46dd1d
deadman96385 <seanhoyt963@gmail.com>
2025-04-15 08:31:57 -0500

    Refactor logging implementation
    This commit improves the logging mechanism by:
    
    -   Updating the logging configuration to use `RichHandler` which provides better formatting and more informative output.
    -   Enabling rich tracebacks for more detailed error diagnostics.
    -   Setting the `httpx` logger level to WARNING to reduce verbosity.
    -   Setting the `telegram.ext` logger level to INFO.

--- dumpyarabot/__init__.py
+++ dumpyarabot/__init__.py
@@ -3,7 +3,8 @@ import logging
 from rich.logging import RichHandler
 
 FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-logging.basicConfig(level=logging.INFO, format=FORMAT, handlers=[RichHandler()])
+logging.basicConfig(level=logging.INFO, format=FORMAT, handlers=[RichHandler(rich_tracebacks=True)])
 logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("telegram.ext").setLevel(logging.INFO)
 
 logger = logging.getLogger("rich")
